e80dce9538e6cacf55513466532d5b6b8c5ff106
kashwath <32027777+kashwath@users.noreply.github.com>
2017-11-03 12:39:45 -0400

    LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.servlet4preview.RequestDispatcher;
 
-/**
- * Servlet implementation class LoginServlet
- */
+
 @WebServlet("/login.do")
 public class LoginServlet extends HttpServlet {
 
@@ -21,37 +19,34 @@ public class LoginServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		//request.getRequestDispatcher("/WEB-INF/Views/index.jsp").forward(request, response);
-
-
-
-
+		
+	
 		request.getRequestDispatcher("/WEB-INF/Views/login.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
 		String name=request.getParameter("name");
 		request.setAttribute("name",name );
-
+		
 		String password=request.getParameter("password");
 		request.setAttribute("password", password);
-
+		
 		/*System.out.println("username" + name);
 		System.out.println("password" + password);*/
-
-		if(loginDetails.Validate(name, password)) {
+		
+		if(loginDetails.Validate(name, password)==true) {
 			request.getRequestDispatcher("/WEB-INF/Views/welcome.jsp").forward(request, response);
 		}
-		else {
+		else{
 			System.out.print("Wrong username or password");
+			//SendSMS.sendMessage("Wrong password","+17047633905");
 			request.getRequestDispatcher("/WEB-INF/Views/login.jsp").forward(request, response);
 		}
-
-
+		
+		
 	}
 
 }
